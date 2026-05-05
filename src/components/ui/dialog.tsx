797446fcb1d4af6f7c3f5f133a8cbf780c06894e
dg-fuadmhnr <fuad@webdeveloper.dgtraffic.com>
2026-05-05 22:38:00 +0700

    fix: dialog always focus on close edit

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -60,7 +60,7 @@ function DialogContent({
 
     queueMicrotask(() => {
       const focusable = contentRef.current?.querySelector<HTMLElement>(
-        '[data-autofocus], textarea:not([disabled]), input:not([disabled]), select:not([disabled]), [href], button:not([disabled]), [tabindex]:not([tabindex="-1"])',
+        '[data-autofocus], textarea:not([disabled]), input:not([disabled]), select:not([disabled]), [href], [tabindex]:not([tabindex="-1"])',
       )
       focusable?.focus()
     })
